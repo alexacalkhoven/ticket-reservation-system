c01a5da3c451d9a97d43abbe4ac213f5f0e311b5
Noah Bradley <nbradleyjoe19@gmail.com>
2020-11-25 09:46:14 -0700

    Added main frame and gui controller classes. Shouldnt need a communicate class but we shall see...

---
+++ GUIController.java
@@ -0,0 +1,7 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GUIController {
+    private MainFrame frame;
+    private DataCommunicator communicator;
+}
