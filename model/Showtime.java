aed7d2f7f2b06d28f2bec20cc95d2ccc9b45ed7d
Trevor Brown <59982025+trevorbrown18@users.noreply.github.com>
2020-11-29 12:35:13 -0700

    Add files via upload
    Completed the cancel ticket function in GUIController. Changed Ticket Classes cancelTicket function to only need a username to be passed. - Now it used the UserController to find if the username matches a registered users account and then chooses the type accordingly for the receipts.

---
+++ model/Showtime.java
@@ -0,0 +1,95 @@
+package main.model;
+
+/**
+ * Holds information on all the available showtimes for specific movies.
+ * This class is used to pass information between GUI and controllers.
+ * @author Alexa Calkhoven
+ * @author Madisson Carle
+ * @author Trevor Brown
+ * @author Noah Bradley
+ * 
+ *
+ */
+public class Showtime {
+
+	private String time; // keep this as a string for now since we only really have to print it out.
+	private int showtimeId;
+	private int movieId;
+
+	/**
+	 * Constructs showtime
+	 * @param showtimeId Id of showtime
+	 * @param time time of showtime
+	 * @param movieId movie at showtime
+	 */
+	public Showtime(int showtimeId, String time, int movieId) {
+		this.showtimeId = showtimeId;
+		this.time = time;
+		this.movieId = movieId;
+	}
+
+	/**
+	 * constructor
+	 */
+	public Showtime() {
+
+	}
+
+	/**
+	 * Converts showtime to string for GUI
+	 * @return Returns the string result
+	 */
+	@Override
+	public String toString() {
+		return "time:" + time + " showtimeId: " + showtimeId + " movieId: " + movieId;
+	}
+
+	/**
+	 * Gets time for showtime
+	 * @return Returns time as a string
+	 */
+	public String getTime() {
+		return time;
+	}
+
+	/**
+	 * Sets time for showtime
+	 * @param time Time to be set
+	 */
+	public void setTime(String time) {
+		this.time = time;
+	}
+
+	/**
+	 * Gets the showtime Id
+	 * @return Returns the int showtime ID
+	 */
+	public int getShowtimeId() {
+		return showtimeId;
+	}
+
+	/**
+	 * Sets the showtime ID
+	 * @param showtimeId The ID to be set
+	 */
+	public void setShowtimeId(int showtimeId) {
+		this.showtimeId = showtimeId;
+	}
+
+	/**
+	 * Gets the movie ID for the showtime
+	 * @return Returns the int Movie ID
+	 */
+	public int getMovieId() {
+		return movieId;
+	}
+
+	/**
+	 * Sets the movie ID for the showtime
+	 * @param movieId The movie ID to be set
+	 */
+	public void setMovieId(int movieId) {
+		this.movieId = movieId;
+	}
+
+}
