6bdbf758d233fece1b2a71e5f652870a5e37671e
Trevor Brown <59982025+trevorbrown18@users.noreply.github.com>
2020-11-29 19:15:32 -0700

    Update Ticket.java
    Updated the valid cancel function so now it compares to  showtimes date rather than string.

--- main/model/Ticket.java
+++ main/model/Ticket.java
@@ -57,10 +57,12 @@ public class Ticket {
 	 * Converts Ticket class to a string to be passed over to the GUI.
 	 * @return Returns the ticket class as a String.
 	 */
+	//NOTE :  reformatted so the time purchased would be readable. And got rid of other values that would add clutter in GUI area.
 	@Override
 	public String toString() {
-		return "ticketId: " + ticketId + " seatId: " + seatId + " showtimeId:" + showtimeId + " paymentId:" + paymentId
-				+ " timePurchased:" + timePurchased.toString() + "\n";
+		return "Ticket ID: " + ticketId 
+				+ " timePurchased:" + timePurchased.getMonth() + "/" + timePurchased.getDate() + "/"
+				+ (timePurchased.getYear() + 1900) + "\n";
 	}
 
 	//getters and setters
@@ -112,7 +114,19 @@ public class Ticket {
 	 */
 	public boolean validCancel() {
 
-		long timeDifMS = new java.util.Date().getTime() - timePurchased.getTime(); // get current time in ms and
+		ShowtimeController sc = new ShowtimeController();
+		
+		
+		Showtime st = sc.getShowtime(showtimeId);
+		
+		if(st ==null) { //database error, so cancellation will not work.
+			return false; 
+		}
+		
+		
+		
+		
+		long timeDifMS = new java.util.Date().getTime() - st.getTimeOfShow().getTime(); // get current time in ms and
 																					// subtract purchase time in ms.
 
 		int timeDifHrs = (int) timeDifMS / 3600000; // get the integer time difference in hours
@@ -142,6 +156,7 @@ public class Ticket {
 		PaymentController pc = new PaymentController();
 
 		UserController uc = new UserController(); //to see if username matches a registered user or not. 
+			
 		
 		// check if cancellation is valid
 		if (validCancel() == false) {
@@ -153,6 +168,7 @@ public class Ticket {
 		String message = "";
 		Date currentDate = new Date();
 		Payment payment = pc.getPayment(paymentId);
+	
 		if (payment == null) {
 			System.err.println("Error in retrieving payment information for refund.");
 		}
@@ -168,7 +184,7 @@ public class Ticket {
 		for(RegisteredUser ru: uc.getRegisteredUsers()) {
 			if(ru.getUsername().equals(username)) { //set the type to registerd user and exit the loop
 				type =1;
-				break;
+				return;
 			}
 		}
 
@@ -243,7 +259,7 @@ public class Ticket {
 				+ paymentId + "\n";
 		//message += "Seat: row " + seat.getRow() + " column " + seat.getCol() + ", screen " + seat.getScreen() + "\n"; // COMMENT BACK IN WHEN WE HAVE SETTERS AND GETTERS
 		message += "Movie: " + movie.getName() + "\n";
-		message += "Time: " + st.getTime() + "\n";
+		message += "Time: " + st.getTimeOfShow() + "\n";
 		message += "Date Purchased: " + timePurchased.getMonth() + "/" + timePurchased.getDate() + "/"
 				+ (timePurchased.getYear() + 1900) + "\n";
 		message += "Cost: $" + payment.amount + ". Show this email as your ticket.\n";
